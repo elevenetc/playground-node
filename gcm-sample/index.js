42d3273076872a764d8c587d71a9d3860a83bd65
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-20 00:38:00 +0200

    Add http server implementation

--- gcm-sample/index.js
+++ gcm-sample/index.js
@@ -4,15 +4,30 @@
 
 const gcm = require('node-gcm');
 const settings = require('./local-settings');
+const http = require('http');
+const urlUtils = require('url');
+const sender = new gcm.Sender(settings.apiKey);
+const regTokens = ['dAW5eBptUpk:APA91bF337tn_U0VsSZ1EcCbe7lOIaFynAu8Mx6mFxthURX4EHSovDUbU9AUKFQ-_lkkxMd8eO1xiC2tx0B-lG3mtpKhJh5yqpEI5-hOmOZtzk9yLEYUTuUdNRGWV7kCCFVWeBzZpknJ'];
+
+
+function handleRequest(request, response) {
+	const queryMap = urlUtils.parse(request.url, true).query;
+	response.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	response.end('{}');
+}
 
 function run() {
+	http.createServer(handleRequest).listen(8080, function () {
+		console.log("Server listening on: http://localhost:%s", 8080);
+	});
+}
 
+function sendMessage() {
 	var message = new gcm.Message({
 		data: {key1: 'msg1'}
 	});
-
-	var sender = new gcm.Sender(settings.apiKey);
-	var regTokens = ['dAW5eBptUpk:APA91bF337tn_U0VsSZ1EcCbe7lOIaFynAu8Mx6mFxthURX4EHSovDUbU9AUKFQ-_lkkxMd8eO1xiC2tx0B-lG3mtpKhJh5yqpEI5-hOmOZtzk9yLEYUTuUdNRGWV7kCCFVWeBzZpknJ'];
 
 	sender.send(message, {registrationTokens: regTokens}, function (err, response) {
 		if (err) {
@@ -21,8 +36,6 @@ function run() {
 			console.log('response:' + response);
 		}
 	});
-
-	process.exit();
 }
 
 run();
