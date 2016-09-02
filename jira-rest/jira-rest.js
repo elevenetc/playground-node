7b9a8e651c3e860e852d218703ac8b348e5de4eb
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-09-02 17:36:05 +0200

    Add last week hours counting

--- jira-rest/jira-rest.js
+++ jira-rest/jira-rest.js
@@ -3,6 +3,7 @@
  */
 const utils = require('../utils/utils');
 const request = require('request');
+const moment = require('moment');
 
 class JiraRest {
 
@@ -10,13 +11,22 @@ class JiraRest {
 		this.jiraEnpoint = utils.checkEnvVar('JIRA_ENDPOINT', true);//e.g. https://jdog.atlassian.com/
 		this.username = utils.checkEnvVar('JIRA_USERNAME', true);
 		this.password = utils.checkEnvVar('JIRA_PASSWORD', true);
+		this.dateFormat = 'YYYY-MM-DD';
+		this.monday = '';
+		this.friday = '';
+		this.initDates();
 		this.getHours();
+
+		console.log('monday: ' + this.monday);
+		console.log('friday: ' + this.friday);
 	}
 
 	getHours() {
 
+		const ref = this;
+
 		request({
-			url: this.jiraEnpoint + 'rest/tempo-timesheets/3/worklogs/?username=' + this.username + '&dateFrom=2016-06-01&dateTo=2016-06-01',
+			url: this.jiraEnpoint + 'rest/tempo-timesheets/3/worklogs/?username=' + this.username + '&dateFrom=' + this.monday + '&dateTo=' + this.friday,
 			method: 'GET',
 			followRedirect: true,
 			headers: {
@@ -27,13 +37,37 @@ class JiraRest {
 				console.log('error');
 			} else {
 				console.log('status: ' + response.statusCode + ' message: ' + response.statusMessage);
-				console.log(body);
+				let totalHours = ref.getTotalHours(JSON.parse(body));
+				console.log('Total hours: ' + totalHours);
 			}
 		});
 	}
 
 	getAuthHeader() {
 		return new Buffer(this.username + ':' + this.password).toString('base64');
+	}
+
+	initDates() {
+		let day = '';
+		let index = 1;
+		let fridayIndex;
+		while (day != 'Fri') day = moment().subtract(index++, 'days').format('ddd');
+		fridayIndex = --index;
+		this.friday = moment().subtract(fridayIndex, 'days').format(this.dateFormat);
+		this.monday = moment().subtract(fridayIndex + 4, 'days').format(this.dateFormat);
+	}
+
+	getTotalHours(response) {
+		let result = 0;
+		for (let i = 0; i < response.length; i++) {
+			let seconds = response[i].timeSpentSeconds;
+			result += seconds / 60 / 60;
+		}
+		return result;
+	}
+
+	getEndDate() {
+		return moment().subtract(3, 'days').format(this.dateFormat);
 	}
 }
 
