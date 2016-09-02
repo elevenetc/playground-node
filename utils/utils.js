52113cb7e965563c9a0d429886128f1a3147c5fc
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-09-02 16:26:31 +0200

    Add utility file

---
+++ utils/utils.js
@@ -0,0 +1,14 @@
+/**
+ * Created by eugene.levenetc on 02/09/16.
+ */
+module.exports = {
+	checkEnvVar: function (envName, crash) {
+		if (!process.env[envName]) {
+			const message = 'Error. No defined env var: ' + envName;
+			if (crash) throw new Error(message);
+			else console.log(message);
+		} else {
+			return process.env[envName];
+		}
+	}
+};
