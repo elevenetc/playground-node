c0fd2011af80e89d8190e5241366fda0e5e5ad84
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-09-02 16:28:06 +0200

    Add jira-rest sample

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
  * Created by eleven on 23/07/2016.
  */
-require('./redis')();
+//require('./redis')();
+require('./jira-rest/jira-rest')();
---
+++ jira-rest/jira-rest.js
@@ -0,0 +1,40 @@
+/**
+ * Created by eugene.levenetc on 02/09/16.
+ */
+const utils = require('../utils/utils');
+const request = require('request');
+
+class JiraRest {
+
+	constructor() {
+		this.jiraEnpoint = utils.checkEnvVar('JIRA_ENDPOINT', true);//e.g. https://jdog.atlassian.com/
+		this.username = utils.checkEnvVar('JIRA_USERNAME', true);
+		this.password = utils.checkEnvVar('JIRA_PASSWORD', true);
+		this.getHours();
+	}
+
+	getHours() {
+
+		request({
+			url: this.jiraEnpoint + 'rest/tempo-timesheets/3/worklogs/?username=eugene.levenetc&dateFrom=2016-06-01&dateTo=2016-06-01',
+			method: 'GET',
+			followRedirect: true,
+			headers: {
+				'Authorization': 'Basic ' + this.getAuthHeader()
+			}
+		}, function (error, response, body) {
+			if (error) {
+				console.log('error');
+			} else {
+				console.log('status: ' + response.statusCode + ' message: ' + response.statusMessage);
+				console.log(body);
+			}
+		});
+	}
+
+	getAuthHeader() {
+		return new Buffer(this.username + ':' + this.password).toString('base64');
+	}
+}
+
+module.exports = new JiraRest();
